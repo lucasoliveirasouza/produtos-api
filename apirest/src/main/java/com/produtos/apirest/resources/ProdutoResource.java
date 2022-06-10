87adb6817f612c14450b38d253d454e848c07ca4
lucasoliveirasouza <>
2022-06-09 21:53:46 -0300

    Criar PUT e DELETE #7 #8

--- apirest/src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ apirest/src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -27,4 +27,15 @@ public class ProdutoResource {
     public Produto salvaProduto(@RequestBody Produto produto) {
         return produtoRepository.save(produto);
     }
+
+    @DeleteMapping("/produto")
+    public void deletaProduto(@RequestBody Produto produto) {
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("/produto")
+    public Produto atualizaProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
 }
